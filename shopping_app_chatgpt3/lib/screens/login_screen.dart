d503ce27d3d81892a6abdafaff9a02533795e52d
Josip Domazet <josip.domazet.corp@gmail.com>
2024-04-14 01:54:00 +0200

    chatgpt3.5: #2

---
+++ shopping_app_chatgpt3/lib/screens/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/productListing');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ shopping_app_chatgpt3/lib/screens/product_listing_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductListingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Listing'),
+      ),
+      body: Center(
+        child: Text('Product Listing Screen'),
+      ),);
+  }
+}

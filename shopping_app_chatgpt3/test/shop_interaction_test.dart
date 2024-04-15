7fc687274e4f217d2afa8f5229e8405b293ab937
Josip Domazet <josip.domazet.corp@gmail.com>
2024-04-15 21:50:39 +0200

    chatgpt3.5: #T11

--- shopping_app_chatgpt3/test/shop_interaction_test.dart
+++ shopping_app_chatgpt3/test/shop_interaction_test.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shopping_app_chatgpt3/main.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app_chatgpt3/models/product.dart';
+import 'package:shopping_app_chatgpt3/providers/cart_provider.dart';
+import 'package:shopping_app_chatgpt3/screens/cart_screen.dart';
 
 void main() {
   testWidgets('Remove item from shopping cart', (WidgetTester tester) async {
-    // Build the widget tree
-    await tester.pumpWidget(MyApp());
+    final cartProvider = CartProvider();
+    cartProvider.addProductToCart(
+        Product(id: 1, name: 'Test Product', imageUrl: 'url', price: 9.99));
+
+    await tester.pumpWidget(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return cartProvider;
+        }),
+      ],
+      child: MaterialApp(home: CartScreen()),
+    ));
 
     // Tap the "Remove" button for a product
     await tester.tap(find.byIcon(Icons.remove).first);
     await tester.pump();
 
     // Verify that the product is removed from the cart
-    expect(find.text('Product Name'), findsNothing);
+    expect(find.text('Test Product'), findsNothing);
   });
 }

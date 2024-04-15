7c6a78036d3d9a5200eb7f4acae7fd85cceecf42
Josip Domazet <josip.domazet.corp@gmail.com>
2024-04-15 21:40:10 +0200

    chatgpt3.5: #T10

--- shopping_app_chatgpt3/test/shop_cart_test.dart
+++ shopping_app_chatgpt3/test/shop_cart_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shopping_app_chatgpt3/main.dart';
 
@@ -5,6 +6,8 @@ void main() {
   testWidgets('AppBar title is correct', (WidgetTester tester) async {
     // Build the widget tree
     await tester.pumpWidget(MyApp());
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
 
     // Verify the presence and correctness of the title in the AppBar
     expect(find.text('Product Listing'), findsOneWidget);

1beedbceca973c993fb544699a926a7d4d07d02d
Josip Domazet <josip.domazet.corp@gmail.com>
2024-04-18 00:36:42 +0200

    e4:

--- provider_shopper/lib/screens/catalog.dart
+++ provider_shopper/lib/screens/catalog.dart
@@ -35,14 +35,7 @@ class _AddButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // The context.select() method will let you listen to changes to
-    // a *part* of a model. You define a function that "selects" (i.e. returns)
-    // the part you're interested in, and the provider package will not rebuild
-    // this widget unless that particular part of the model changes.
-    //
-    // This can lead to significant performance improvements.
     var isInCart = context.select<CartModel, bool>(
-      // Here, we are only interested whether [item] is inside the cart.
       (cart) => cart.items.contains(item),
     );
 
@@ -50,11 +43,7 @@ class _AddButton extends StatelessWidget {
       onPressed: isInCart
           ? null
           : () {
-              // If the item is not in cart, we let the user add it.
-              // We are using context.read() here because the callback
-              // is executed whenever the user taps the button. In other
-              // words, it is executed outside the build method.
-              var cart = context.read<CartModel>();
+              var cart = context.watch<CartModel>();
               cart.add(item);
             },
       style: ButtonStyle(

b1b2cbdcb4c18e5ece2bec3ba65026ee652487ca
Josip Domazet <josip.domazet.corp@gmail.com>
2024-04-19 23:45:04 +0200

    chatgpt4.0: #T10: made it work for web

--- shopping_app_chatgpt4/lib/test_utils.dart
+++ shopping_app_chatgpt4/lib/test_utils.dart
@@ -1,5 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 bool isTestMode() {
+  if (kIsWeb) {
+    return false;
+  }
+
   return Platform.environment.containsKey('FLUTTER_TEST');
 }
